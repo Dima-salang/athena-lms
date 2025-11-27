0bd1e28e9b9ee33e8e807a8f2478783193e4e44a
Dima-salang <luisgabrielle1026@gmail.com>
2025-11-27 18:23:50 +0800

    repos for submission and studentanswer

---
+++ src/main/java/com/athena/lms/athena_lms/repository/StudentAnswerRepository.java
@@ -0,0 +1,8 @@
+package com.athena.lms.athena_lms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.athena.lms.athena_lms.model.submission.StudentAnswer;
+
+public interface StudentAnswerRepository extends JpaRepository<StudentAnswer, Long> {
+
+}
